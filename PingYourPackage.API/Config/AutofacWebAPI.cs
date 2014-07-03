a8e42c7298df38e4d8d4b23cd64d15b1be801ccb
FollowJack <d.horvatic@outlook.com>
2014-07-03 21:27:21 +0200

    Add project structure

---
+++ PingYourPackage.API/Config/AutofacWebAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace PingYourPackage.API.Config
+{
+    public class AutofacWebAPI
+    {
+        //for hosting initialisezing
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config,RegisterServices(new ContainerBuilder()));
+        }
+        //for integration tests
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //Registration
+
+            return builder.Build();
+        }
+    }
+}
---
+++ PingYourPackage.Domain/Entities/Core/EntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingYourPackage.Domain.Entities.Core
+{
+    public class EntityRepository<T> : IEntityRepository<T> : where T : class, IEntity, new()
+    {
+        private readonly DbContext _entitiesContext;
+
+        public EntityRepository(DbContext entitiesContext)
+        {
+            if (entitiesContext == null)
+                throw new ArgumentNullException("entitiesContext");
+
+            _entitiesContext = entitiesContext;
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _entitiesContext.Set<T>();
+        } 
+    }
+}
