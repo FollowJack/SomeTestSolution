ee832b5487f479cb97ce2432e2d2aefe140842fd
FollowJack <d.horvatic@outlook.com>
2014-07-03 21:25:57 +0200

    Add generic repository

---
+++ PingYourPackage.API.WebHost/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using PingYourPackage.API.Config;
+
+namespace PingYourPackage.API.WebHost
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            RouteConfig.RegisterRoutes(config);
+            WebAPIConfig.Configure(config);
+            AutofacWebAPI.Initialize(config);
+        }
+    }
+}
--- PingYourPackage.Domain/Entities/Core/IEntity.cs
+++ PingYourPackage.Domain/Entities/Core/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PingYourPackage.API
+namespace PingYourPackage.Domain.Entities.Core
 {
-    public class Class1
+    public interface IEntity
     {
     }
 }
