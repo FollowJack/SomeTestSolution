14b8d20653e40a49db49e3908640db73b154c986
FollowJack <d.horvatic@outlook.com>
2014-07-03 22:59:15 +0200

    Add Websecurity to domain

---
+++ PingYourPackage.Domain/Entities/Configuration/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Migrations.Infrastructure;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace PingYourPackage.Domain.Entities.Migrations
+{
+    public static class DbInitializer
+    {
+        public static void Initialize()
+        {
+            InitializeDbConnection();
+            AddRoles();
+        }
+
+        private static void AddRoles()
+        {
+            if (!Roles.RoleExists("Admin"))
+                Roles.CreateRole("Admin");
+            if (!Roles.RoleExists("Employee"))
+                Roles.CreateRole("Employee");
+            if (!Roles.RoleExists("Affiliate"))
+                Roles.CreateRole("Affiliate");
+        }
+
+        private static void InitializeDbConnection()
+        {
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "User", "Key", "Name", true);
+        }
+    }
+}
---
+++ PingYourPackage.Domain/Entities/PingYourPackageModelContext.Custom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PingYourPackage.Domain.Entities
+{
+    public partial class Entities
+    {
+
+        #region Request-wide Singleton
+
+        private const string CONTEXTKEY_DATACONTEXT = "{CD01E2F2-3893-4580-8D19-9C948B6F33BD}";
+
+        public static Entities Current
+        {
+            get
+            {
+                Entities result =
+                    HttpContext.Current.Items[CONTEXTKEY_DATACONTEXT] as Entities;
+                if (result == null)
+                {
+                    result = new Entities();
+                    HttpContext.Current.Items.Add(CONTEXTKEY_DATACONTEXT, result);
+                }
+                return result;
+            }
+        }
+
+        #endregion
+    }
+}
